c84baf6b875895b70a88b40b31ca8dae38e4f551
yuzequn095 <yuzequn095@gmail.com>
2026-02-22 14:06:43 -0800

    feat(api): add prisma service using v7 postgres adapter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // Prisma v7 requires a non-empty PrismaClientOptions (adapter or accelerateUrl).
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
