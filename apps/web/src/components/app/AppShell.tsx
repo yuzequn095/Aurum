3ad82006c2abb8d38183caa3a09e7100dd4b58a3
yuzequn095 <yuzequn095@gmail.com>
2026-03-05 10:49:44 -0800

    chore(web): refine full-bleed layout

--- apps/web/src/components/app/AppShell.tsx
+++ apps/web/src/components/app/AppShell.tsx
@@ -4,15 +4,13 @@ import { Topbar } from '@/components/app/Topbar';
 
 export function AppShell({ children }: PropsWithChildren) {
   return (
-    <div className='relative min-h-screen w-full overflow-hidden bg-aurum-bg aurum-noise'>
+    <div className='relative min-h-screen w-full overflow-hidden aurum-noise'>
       <div className='absolute inset-0 aurum-app-bg' />
       <div className='relative flex min-h-screen w-full'>
         <SidebarNav />
         <div className='min-w-0 flex-1'>
           <Topbar />
-          <main className='px-4 py-6 sm:px-6 lg:px-8'>
-            <div className='mx-auto w-full max-w-[1240px]'>{children}</div>
-          </main>
+          <main className='min-h-[calc(100vh-74px)] w-full'>{children}</main>
         </div>
       </div>
     </div>
